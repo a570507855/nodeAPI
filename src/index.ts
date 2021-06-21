2bc6bda9361bce785d7c968bdf9b3c94cf458c06
许亦勇 <570507855@qq.com>
2021-06-21 18:14:03 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,17 @@ import { useContainer, useExpressServer } from 'routing-controllers';
 import { HttpPostController, HttpGetController } from './lib/server/controller'
 import { Log } from './lib/os/log';
 import Container from 'typedi';
-
+const cors = require('cors');
 const app = express();
+const corsOptions = {
+  'orgin': '*',
+  'methods': 'POST,GET,OPTIONS',
+  'maxAge': 2592000
+}
+app.use(cors(corsOptions));
 useContainer(Container);
 useExpressServer(app, { controllers: [HttpPostController, HttpGetController] }).listen(8888, () => {
-  Log.debug('服务已启动：', 'http://127.0.0.1:8888/')
+  Log.debug('服务已启动：', 'localhost:8888/')
 });
 
 
